a8f6facc909866b50812d37735c50e483f415d45
ken_global <ken_global@qq.com>
2018-09-28 11:21:47 +0800

    基于redis实现工程间的单点登录（sso工程、首页工程）

---
+++ shop_sso/src/main/java/com/qf/shop/shop_sso/ShopSsoApplication.java
@@ -0,0 +1,12 @@
+package com.qf.shop.shop_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSsoApplication.class, args);
+	}
+}
